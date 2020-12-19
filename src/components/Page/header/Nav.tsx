28921f6aa01c7e8c3147f8fee9edb4c378bd6f4e
blazej-k <blazejkania1@gmail.com>
2020-12-19 17:21:00 +0100

    remove animated switch

--- src/components/Page/header/Nav.tsx
+++ src/components/Page/header/Nav.tsx
@@ -68,10 +68,10 @@ const Nav: FC = () => {
                     <NavLink to='/courses' className='link' exact onClick={toogleNav}><BiListPlus style={{fontSize: '120%'}}/>Buy course</NavLink>
                 </li>
                 <li>
-                    <NavLink to='/myCourses' className='link' exact onClick={toogleNav}><CgPlayListCheck style={{fontSize: '120%'}}/>My courses</NavLink>
+                    <NavLink to='/myCourses' className='link' exact onClick={toogleNav} ><CgPlayListCheck style={{fontSize: '120%'}}/>My courses</NavLink>
                 </li>
                 <li>
-                    <NavLink to='/contact' className='link' exact onClick={toogleNav}><MdContactPhone/><span>Contact</span></NavLink>
+                    <NavLink to='/contact' className='link' exact onClick={toogleNav} ><MdContactPhone/><span>Contact</span></NavLink>
                     </li>
                 {isAdmin && <li>
                     <NavLink to='/admin' className='link' exact onClick={toogleNav}><RiContactsLine style={{fontSize: '90%'}}/><span>Admin</span></NavLink>
